d1c2c160698a5e8fbafb67ff7f42ff7ce345dbe1
Igor <eng.igorwatanabe@gmail.com>
2023-03-30 15:19:49 -0300

    item 2 finalizado

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" className="solar-system">
+        <h1>Sistema Solaaaar</h1>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
