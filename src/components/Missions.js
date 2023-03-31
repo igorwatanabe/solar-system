69d6e7b6c9786023dd51bf130b32ecec0195f307
Igor <eng.igorwatanabe@gmail.com>
2023-03-31 20:17:03 -0300

    itens 9 e 10 finalizados

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section>
+          {missions.map((missoes) => (
+            <MissionCard
+              key={ missoes.name }
+              name={ missoes.name }
+              year={ missoes.year }
+              country={ missoes.country }
+              destination={ missoes.destination }
+            />
+          ))}
+        </section>
       </div>
     );
   }
